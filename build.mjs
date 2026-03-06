41b45f21e1d7754c370800a19400bf6d477aeb9e
sun <467169530@qq.com>
2026-03-06 15:51:30 +0800

    修改了目录

--- build.mjs
+++ build.mjs
@@ -7,7 +7,7 @@ import { readFileSync, writeFileSync } from 'fs';
 await build({
   entryPoints: ['src/main.ts'],
   bundle: true,
-  outfile: 'dist/main.js',
+  outfile: 'main.js',
   platform: 'node',
   target: 'node16',
   external: ['fs', 'path', 'electron', 'obsidian'],
@@ -17,14 +17,3 @@ await build({
   treeShaking: true,
   legalComments: 'none',
 });
-
-// 复制文件
-try {
-    const Content = readFileSync('src/styles.css', 'utf8');
-    const Content2 = readFileSync('manifest.json', 'utf8');
-    writeFileSync('dist/styles.css', Content);
-    writeFileSync('dist/manifest.json', Content2);
-    console.log('✅ 文件复制成功');
-} catch {
-    console.log('❌ 文件复制失败:');
-}
